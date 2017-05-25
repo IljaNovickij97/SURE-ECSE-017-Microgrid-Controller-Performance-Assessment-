bda858dff8e9dc709ea2ea0a753483c80d98a83e
a-linux-user <antonia@astraea.net>
2017-05-25 13:40:39 -0400

    Added renewable.py and running_cost.py

---
+++ running_cost.py
@@ -0,0 +1,19 @@
+# running cost evaluation depends on unit commitment and generator output
+# unit commitment = total time for P_dem =/= 0
+
+# import data
+# cost of generation
+def genCost():
+    pass
+
+class TemplateMetric(object):
+    def __init__(self):
+        # Put stuff here if using variables throughout the class i.e. something that's calculated for all of the methods
+        # below
+        pass
+
+    @staticmethod
+    def template_calculation_plot(data, canvas):
+        # Do calculations here
+        #canvas.axes.plot(x,y)
+        pass
